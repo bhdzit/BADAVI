c8591645792dbbc32a433ecb9df5a34c08ae7694
Bryan Eliut Hernandez Moran <bhdzit97@gmail.com>
2019-05-13 18:45:44 -0500

    Modificaion Coctel

--- app/src/main/java/com/bhdz/badavi/AddCoctel.java
+++ app/src/main/java/com/bhdz/badavi/AddCoctel.java
@@ -1,4 +1,17 @@
 package com.bhdz.badavi;
 
-public class AddCoctel {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddCoctel extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
--- app/src/main/java/com/bhdz/badavi/Consultas.java
+++ app/src/main/java/com/bhdz/badavi/Consultas.java
@@ -1,4 +1,80 @@
 package com.bhdz.badavi;
 
-public class Consultas {
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Consultas  extends AsyncTask<String, Void, String> {
+    public final String TAG="Consultas";
+    public static final String servidor="http://192.168.201.57/";
+    @Override
+    protected String doInBackground(String... urls) {
+
+        // params comes from the execute() call: params[0] is the url.
+        try {
+            return downloadUrl(urls[0]);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+    // onPostExecute displays the results of the AsyncTask.
+    @Override
+    protected void onPostExecute(String result) {
+
+
+    }
+
+
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.e(TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
 }
